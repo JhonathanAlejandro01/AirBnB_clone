142a6e33e12068be5bb406dc74ea05bd44255f43
Diego <vandeldiegoc@gmail.com>
2020-06-29 18:03:35 -0500

    new class file_storege

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class FileStorage():
+    """class with which we can serializes and deserialize"""
+    __init__(self)
+        """contructor"""
+        self.__file_path = file.json
+        self.__object = {}
+
+
+        def all(self):
+
+        def new(self, obj):
+
+        def save(self):
+
+        def reload(self)
